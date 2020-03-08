6fc26a7f038ab23ed5f991cda37aa4a383ece0ea
valentina177 <60409104+valentina177@users.noreply.github.com>
2020-03-08 16:35:53 -0400

    meme2

--- meme2/sketch.js
+++ meme2/sketch.js
@@ -62,10 +62,11 @@ function draw() {
 
 	if (counter == 0) {
 		noTint();
-		image(sleepingImage, 170, 60, width/2, height/2);
-		text('I am sleeping do NOT click on me', 320, 280);
+		image(sleepingImage, 170, 2, width/2, height/1);
+		text('I am sleeping.', 120, 50);
+		text('Do NOT click on me.', 500, 290);
 	} else if (counter == 1) {
-		image(foxImage,170, 60, width/2, height/2);
+		image(foxImage,170, 2, width/2, height/1);
 		text('You woke me up!',320, 280);
 	}
 
